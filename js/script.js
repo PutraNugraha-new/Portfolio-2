3cd173abebbc7887a1b8e614d6ca3f4fb79ac133
PutraNugraha-New <putranugraha803@gmail.com>
2022-07-26 19:48:33 +0700

    menambahkan efek autoscroll

--- js/script.js
+++ js/script.js
@@ -5,3 +5,10 @@ bg.addEventListener('click', function() {
     kolapse.classList.toggle('bg-light');
 }
 );
+
+const penunjuk = document.querySelector('.penunjuk');
+const target = document.querySelector('.about-me');
+
+penunjuk.addEventListener('click', function(){
+    target.scrollIntoView();
+} );
